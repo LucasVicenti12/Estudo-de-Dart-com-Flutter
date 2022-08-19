ff6f7a2b49f56dd59dc9c8b701b05519a594c51c
Lucas Vicenti <lucasvicenti60@gmail.com>
2022-08-19 11:55:48 -0300

    Componetizacao de widgets

---
+++ lib/AppWidget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ola_mundo/app_controller.dart';
+import 'home_page.dart';
+
+class AppWidget extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: AppController.instance,
+      builder: (context, child) {
+      return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        brightness: AppController.instance.isDarkTheme ? Brightness.dark : Brightness.light,
+      ),
+      home: HomePage(),
+    );
+    },);
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ola_mundo/app_controller.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        actions: [CustomSwitch()],
+      ),
+      body: Center(
+        child: CustomSwitch(),
+      ),
+      floatingActionButton:
+      FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (() {
+          setState(() {
+            counter++;
+          });
+        }),
+      ),
+    );
+  }
+}
+
+class CustomSwitch extends StatelessWidget {
+  const CustomSwitch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      value: AppController.instance.isDarkTheme,
+      onChanged: (value) {
+        AppController.instance.changeTheme();
+      },
+    );
+  }
+}
